7da004802efdb6405130eb75f4a5d0c3433f423c
rabee361 <rha60540@gmail.com>
2024-07-03 18:32:33 +0300

    changes

---
+++ src/assets/elements.ts
@@ -0,0 +1,35 @@
+let timelineElements = [
+    {
+      id: 1,
+      title: "Danac E-Store and Employee Managment App",
+      location: "Freelancer",
+      description:
+        "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+      buttonText: "view project",
+      date: "September 2023 - March 2024",
+      icon: "work",
+    },
+    {
+      id: 2,
+      title: "Al-Noor Haj App",
+      location: "Freelancer",
+      description:
+        "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+      buttonText: "view project",
+      date: "April 2024 - june 2024",
+      icon: "work",
+    },
+    {
+      id: 3,
+      title: "Expense Tracker (Graduation Project)",
+      location: "Al-Baath University",
+      description:
+        "Expense Tracker app built with Flutter and Django integrated with AI tools to help users visualize thier expenses, suggest ways to save money and monitor their account balance. ",
+      buttonText: "view project",
+      date: "January 2024 - July 2024",
+      icon: "collage",
+    },
+
+  ];
+  
+  export default timelineElements;
